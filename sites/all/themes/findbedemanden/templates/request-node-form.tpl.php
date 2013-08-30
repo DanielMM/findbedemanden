54fc9987107c839c04071e7cc31d913a146590ec
DanielMM <moisdaniel7@gmail.com>
2013-08-30 22:27:14 +0200

    Hide labels for the contact info section on the add request form page.

--- sites/all/themes/findbedemanden/templates/request-node-form.tpl.php
+++ sites/all/themes/findbedemanden/templates/request-node-form.tpl.php
@@ -1,5 +1,5 @@
 <?php
-//dpm($form);
+dpm($form);
 $form['field_ceremony']['#attributes']['class'][] = 'large-6';
 $form['field_method']['#attributes']['class'][] = 'large-6';
 $form['field_name']['#attributes']['class'][] = 'large-6';
@@ -8,6 +8,14 @@ $form['field_city']['#attributes'] = array('class' => array('large-3', 'large-of
 $form['field_email']['#attributes']['class'][] = 'large-6';
 $form['field_telephone']['#attributes']['class'][] = 'large-6';
 
+// Hide labels for the contact info
+$form['field_name']['und']['0']['value']['#title'] = '';
+$form['field_post_nr']['und']['0']['value']['#title'] = '';
+$form['field_city']['und']['0']['value']['#title'] = '';
+$form['field_email']['und']['0']['value']['#title'] = '';
+$form['field_telephone']['und']['0']['value']['#title'] = '';
+
 $form['actions']['submit']['#attributes'] = array('class'=> array( 'large-6'));
+$form['actions']['submit']['#value'] = t('Modtag dine gratis tilbud');
 
 print drupal_render_children($form);
