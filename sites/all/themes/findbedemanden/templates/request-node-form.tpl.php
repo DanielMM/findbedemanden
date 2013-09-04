17796ff2056bc66552c956644c2251360ef22860
DanielMM <moisdaniel7@gmail.com>
2013-09-04 07:46:52 +0200

    Offset left form fields, space out blocks in right sidebar.

--- sites/all/themes/findbedemanden/templates/request-node-form.tpl.php
+++ sites/all/themes/findbedemanden/templates/request-node-form.tpl.php
@@ -1,12 +1,14 @@
 <?php
 //dpm($form);
-$form['field_ceremony']['#attributes']['class'][] = 'large-6';
-$form['field_method']['#attributes']['class'][] = 'large-6';
-$form['field_name']['#attributes']['class'][] = 'large-6';
-$form['field_post_nr']['#attributes'] = array('class' => array('large-2'));
-$form['field_city']['#attributes'] = array('class' => array('large-3', 'large-offset-1'));
-$form['field_email']['#attributes']['class'][] = 'large-6';
-$form['field_telephone']['#attributes']['class'][] = 'large-6';
+$form['field_ceremony']['#attributes'] = array('class' => array('large-8', 'large-offset-2'));
+$form['field_method']['#attributes'] = array('class' => array('large-8', 'large-offset-2'));
+$form['field_name']['#attributes'] = array('class' => array('large-8', 'large-offset-2'));
+$form['field_lawyer']['#attributes'] = array('class' => array('large-8', 'large-offset-2'));
+$form['field_wishes']['#attributes'] = array('class' => array('large-8', 'large-offset-2'));
+$form['field_post_nr']['#attributes'] = array('class' => array('large-2', 'large-offset-2'));
+$form['field_city']['#attributes'] = array('class' => array('large-5', 'large-offset-1'));
+$form['field_email']['#attributes'] = array('class' => array('large-8', 'large-offset-2'));
+$form['field_telephone']['#attributes'] = array('class' => array('large-8', 'large-offset-2'));
 
 // Hide labels for the contact info
 /*$form['field_name']['und']['0']['value']['#title'] = '';
@@ -15,7 +17,7 @@ $form['field_city']['und']['0']['value']['#title'] = '';
 $form['field_email']['und']['0']['value']['#title'] = '';
 $form['field_telephone']['und']['0']['value']['#title'] = '';*/
 
-$form['actions']['submit']['#attributes'] = array('class'=> array( 'large-6'));
+$form['actions']['submit']['#attributes'] = array('class' => array('large-8', 'large-offset-2'));
 $form['actions']['submit']['#value'] = t('Modtag dine gratis tilbud');
 
 print drupal_render_children($form);
